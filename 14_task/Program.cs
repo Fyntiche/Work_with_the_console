75f87725dc090f413aff4dc3a93d356bbc86bd73
Ivan <ivaneremov@icloud.com>
2019-11-11 15:37:51 +0300

    Пользователь вводит два числа. Найдите сумму и произведение данных чисел.

---
+++ 14_task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _14_task
+{
+    class Program
+    {
+        /*
+         * Пользователь вводит два числа. Найдите сумму и произведение данных чисел.
+         */
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double summ;
+            double op;
+            Console.WriteLine("Введите 1 число");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите 2 число");
+            b = Convert.ToDouble(Console.ReadLine());
+            summ = a + b;
+            op = a * b;
+            Console.WriteLine($"Сумма = {summ}, произведение = {op}");
+            Console.ReadLine();
+
+        }
+    }
+}
