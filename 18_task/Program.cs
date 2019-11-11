6500c91f6dd21c71e0d97a25a8d9a3dc360a030b
Ivan <ivaneremov@icloud.com>
2019-11-11 16:25:40 +0300

    Пользователь вводит сторону квадрата. Найдите периметр и площадь квадрата.

---
+++ 18_task/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _18_task
+{
+    class Program
+    {
+        /*
+         * Пользователь вводит сторону квадрата. Найдите периметр и площадь квадрата.
+         */
+        static void Main(string[] args)
+        {
+            double a;
+            Console.WriteLine("Введите строну квадрата");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Периметр = {a * 4}, площадь = {a*a}");
+            Console.ReadLine();
+        }
+    }
+}
