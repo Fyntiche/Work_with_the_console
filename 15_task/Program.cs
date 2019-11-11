a03c1c95a13c1179265d08a01bddf5d60cb69a32
Ivan <ivaneremov@icloud.com>
2019-11-11 15:41:58 +0300

    Пользователь вводит число. Выведите на экран квадрат этого числа, куб этого числа.

---
+++ 15_task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _15_task
+{
+    class Program
+    {
+        /*
+         * Пользователь вводит число. Выведите на экран квадрат этого числа, куб этого числа.
+         */
+        static void Main(string[] args)
+        {
+            double a;
+            double a2;
+            double a3;
+            Console.WriteLine("Введите число");
+            a = Convert.ToDouble(Console.ReadLine());
+            a2 = Math.Pow(a, 2);
+            a3 = Math.Pow(a, 3);
+            Console.WriteLine($"Квадрат числа = {a2}, куб числа = {a3}");
+            Console.ReadLine();
+        }
+    }
+}
