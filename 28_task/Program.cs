4aca04693ed0a38085fee1f2d9b44c425b8192f1
Ivan <ivaneremov@icloud.com>
2019-11-13 12:38:35 +0300

    Пользователь вводит сумму вклада в банк и годовой процент. Найдите сумму вклада через 5 лет

---
+++ 28_task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_task
+{
+    class Program
+    {
+        /*
+         * Пользователь вводит сумму вклада в банк и годовой процент. 
+         * Найдите сумму вклада через 5 лет 
+         */
+        static void Main(string[] args)
+        {
+            double summ;
+            double proc;
+            Console.WriteLine("Введите сумму");
+            summ = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите процент вклада");
+            proc = Convert.ToDouble(Console.ReadLine());
+            for (int i = 0; i< 5; i++)
+            {
+                summ += summ * proc/100;
+            }
+            Console.WriteLine($"Сумма через 5 лет = {Math.Round(summ,3)}");
+            Console.ReadLine();
+        }
+    }
+}
