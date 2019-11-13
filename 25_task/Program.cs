0747bf3904f7e6609e3e39667f26f7609ae92d1f
Ivan <ivaneremov@icloud.com>
2019-11-13 10:58:42 +0300

    Пользователь вводит количество недель, месяцев, лет и получает количество дней за это время. Считать, что в месяце 30 дней.

---
+++ 25_task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _25_task
+{
+    class Program
+    {
+        /*
+         * Пользователь вводит количество недель, 
+         * месяцев, лет и получает количество дней 
+         * за это время. Считать, что в месяце 30 
+         * дней.
+         */
+        static void Main(string[] args)
+        {
+            int days;
+            int weeks;
+            int month;
+            int year;
+            Console.WriteLine("Введите колличество недель");
+            weeks = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите колличество месяцев");
+            month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите колличество лет");
+            year = Convert.ToInt32(Console.ReadLine());
+            days = weeks * 7 + month * 30 + year * 12 * 30;
+            Console.WriteLine($"Количество дней = {days}");
+            Console.ReadLine();
+        }
+    }
+}
