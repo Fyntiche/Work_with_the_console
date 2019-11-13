d2741e2853153f35901902312ca4666d1fdab798
Ivan <ivaneremov@icloud.com>
2019-11-13 09:44:23 +0300

    Дано значение температуры в градусах Цельсия. Вывести температуру  в градусах Фаренгейта.

---
+++ 22_task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace _22_task
+{
+    class Program
+    {
+        /*
+         * Дано значение температуры в градусах Цельсия. 
+         * Вывести температуру  в градусах Фаренгейта.
+         */
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            Console.WriteLine("Введите температуру в градусах Цельсия");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = a * 1.8 + 32;
+            Console.WriteLine($"температуру в градусах Фаренгейта = " +
+                $"{b}");
+            Console.ReadLine();
+        }
+    }
+}
+ 
