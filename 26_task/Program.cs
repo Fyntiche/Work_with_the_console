7fcb7ec9489765697fddfabf63b58ce9d29187d5
Ivan <ivaneremov@icloud.com>
2019-11-13 11:04:58 +0300

    Даны две переменных с некоторыми значениями. Поменять местами значения этих переменных

---
+++ 26_task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_task
+{
+    class Program
+    {
+        /*
+         * Даны две переменных с некоторыми значениями. 
+         * Поменять местами значения этих переменных
+         */
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine($"Первое число = {a}, " +
+                $"второе число = {b}");
+            Console.ReadLine();
+        }
+    }
+}
