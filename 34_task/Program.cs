ff866cdba492510780f97c77074edaf2fad70dd7
Ivan <ivaneremov@icloud.com>
2019-11-18 11:34:05 +0300

    Дано число. Если оно меньше 7, то вывести Yes, если больше 10, то вывести No, если равно 9, то вывести Error.

---
+++ 34_task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Введите x");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x > 3)
+            {
+                x += 10;
+            }
+             else
+            {
+                x += 10;
+            }
+            Console.WriteLine($"x={x}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 35_task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Введите x");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x < 7)
+            {
+                Console.WriteLine("Yes");
+            }
+            if (x > 10)
+            {
+                Console.WriteLine("No");
+            }
+            if (x == 9)
+            {
+                Console.WriteLine("Error");
+            }
+            Console.ReadLine();
+        }
+    }
+}
