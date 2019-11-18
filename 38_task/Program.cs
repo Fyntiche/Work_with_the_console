162c00ade335c4079e09a59dfead43aa98783775
Ivan <ivaneremov@icloud.com>
2019-11-18 12:20:53 +0300

    Дано два числа. Вывести yes, если они отличаются на 100, иначе вывести No.

---
+++ 38_task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            Console.WriteLine("Введите 1 число");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2 число");
+            y = Convert.ToInt32(Console.ReadLine());
+            if ((x - y) > 100 || (y - x) > 100)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
