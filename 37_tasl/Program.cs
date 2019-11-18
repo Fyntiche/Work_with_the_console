9d744e03cd2441dce1188d6ee9b31436b9dfa10e
Ivan <ivaneremov@icloud.com>
2019-11-18 12:15:26 +0300

    Дано два числа. Вывести наибольшее из них.

---
+++ 37_tasl/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_tasl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            Console.WriteLine("Введите 1 число");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2 число");
+            y = Convert.ToInt32(Console.ReadLine());
+            if (x>y)
+            {
+                Console.WriteLine($"{x}");
+            }
+            else
+            {
+                Console.WriteLine($"{y}");
+            }
+        }
+    }
+}
