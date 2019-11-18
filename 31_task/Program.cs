61ccc1c7ac28788cbf7b19abfdfa36225437fa1d
Ivan <ivaneremov@icloud.com>
2019-11-18 10:14:04 +0300

    Дан прямоугольник размером 647 x 170. Сколько квадратов со стороной 30 можно вырезать из него?

---
+++ 31_task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 647;
+            int b = 170;
+            int c = 30;
+            int count = 0;
+            count = (a / c) * (b / c);
+            Console.WriteLine($"Вырезанных кваратов = {count}");
+            Console.ReadLine();
+        }
+    }
+}
