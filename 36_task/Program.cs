e1aa118369b139d42a5541a30601e19f12443c83
Ivan <ivaneremov@icloud.com>
2019-11-18 11:55:24 +0300

    Пользователь вводит номер месяца, вывести название месяца.

---
+++ 36_task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36_task
+{
+    class Program
+    {
+        enum Mountse
+        {
+            Январь = 1,
+            Февраль,
+            Март,
+            Апрель,
+            Май,
+            Июнь,
+            Июль,
+            Август,
+            Сентябрь,
+            Октябрь,
+            Ноябрь,
+            Декабрь
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Введите номер месяца");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{(Mountse)(num)}");
+            Console.ReadLine();
+
+        }
+    }
+}
