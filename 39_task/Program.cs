787f7a8ee9bd9e646ddcf24b641f0618b465d7f8
Ivan <ivaneremov@icloud.com>
2019-11-18 12:28:38 +0300

    Даны два числа. Если первое число больше второго, то вывести yes, иначе поменять значения этих переменных и вывести их на экран.

---
+++ 39_task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int c;
+            int y;
+            Console.WriteLine("Введите 1 число");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2 число");
+            y = Convert.ToInt32(Console.ReadLine());
+            if (x>y)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                c = x;
+                x = y;
+                y = c;
+                Console.WriteLine($"1 число ={x}, " +
+                    $"2 число = {y}");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
